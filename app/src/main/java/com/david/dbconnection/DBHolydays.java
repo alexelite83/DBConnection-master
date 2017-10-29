db3decded5a34b3c2bcb516f682a6590f69814d6
Alex Fernandez <alexfernandez@MacBook-Pro-de-Alex.local>
2017-10-29 15:52:33 +0100

    Initial commit

---
+++ app/src/main/java/com/david/dbconnection/DBHolydays.java
@@ -0,0 +1,132 @@
+package com.david.dbconnection;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.Vector;
+
+public class DBHolydays extends SQLiteOpenHelper {
+
+    private Context context;
+
+    private static final String DBNAME = "HolydaysDB";
+    private static final int DAYS_COLUMN_INDEX = 1;
+    private final String HOLYDAYS_TABLE_NAME = "Holydays";
+
+
+    /**
+     * Constructor de la clase
+     *
+     * @param context
+     */
+    public DBHolydays(Context context) {
+        super(context, DBNAME, null, 1);
+        this.context = context;
+    }
+
+    /**
+     * Creará las tablas de la BDD al crear la BDD
+     *
+     * @param db
+     */
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String inst = "CREATE TABLE " + HOLYDAYS_TABLE_NAME + " (_id INTEGER PRIMARY KEY AUTOINCREMENT, Date TEXT, User TEXT)";
+        db.execSQL(inst);
+
+    }
+
+    /**
+     * Acciones a realizar al actualizar una BDD
+     *
+     * @param db
+     * @param oldVersion
+     * @param newVersion
+     */
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    /**
+     * Método para insertar un día de vacaciones a nombre de un usuario
+     *
+     * @param date
+     * @param user
+     */
+    public void insertIntoHolydays(String date, String user) {
+        try {
+            SQLiteDatabase db = getWritableDatabase();
+            String inst = "INSERT INTO " + HOLYDAYS_TABLE_NAME +
+                    " VALUES ( null, '" + date + "', '" + user + "')";
+            db.execSQL(inst);
+        } catch (SQLiteException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    /**
+     * Método para recuperar los días de vacaciones guardados para un usuario
+     *
+     * @param user
+     * @return
+     */
+    public Vector<String> getFromHolydays(String user) {
+        Vector<String> result = new Vector<>();
+        Cursor cursor = null;
+
+        try {
+            // Se selecciona la BDD y se realiza la consulta
+            SQLiteDatabase db = getReadableDatabase();
+            String inst = "SELECT * FROM " + HOLYDAYS_TABLE_NAME + " WHERE User = '" + user + "' ORDER BY _id";
+            cursor = db.rawQuery(inst, null); // Matriz que recibirá el resultado de la consulta
+
+            result.clear();
+
+            // Se copia la columna de los días al vector de Strings
+            if (cursor.moveToFirst()) {
+                do {
+                    String day = cursor.getString(DAYS_COLUMN_INDEX);
+                    result.add(day);
+                } while (cursor.moveToNext());
+            }
+        } catch (SQLiteException ex) {
+            ex.printStackTrace();
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+            return result;
+        }
+
+
+    }
+
+    /**
+     * Método para borrar los días guardados para un usuario
+     *
+     * @param user
+     * @return
+     */
+    public boolean deleteFromHolydays(String user) {
+        boolean deleted = false;
+
+        try {
+            // Se selecciona la BDD y se borran los datos del usuario pasado
+            SQLiteDatabase db = getWritableDatabase();
+            String inst = "DELETE FROM " + HOLYDAYS_TABLE_NAME + " WHERE User = '" + user + "'";
+
+            db.execSQL(inst);
+            deleted = true;
+        } catch (SQLiteException ex) {
+            ex.printStackTrace();
+        } finally {
+            return deleted;
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/david/dbconnection/Main.java
@@ -0,0 +1,98 @@
+package com.david.dbconnection;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
+import java.util.Vector;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class Main extends AppCompatActivity {
+
+    @Bind(R.id.button)
+    Button button;
+    final int MAX_DIAS_VACACIONES = 3;
+
+    public Context context;
+    public DBHolydays dbHolydays;
+    private String user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        context = this;
+
+        // Inicializa la BDD
+        dbHolydays = new DBHolydays(this);
+        user = "David";
+
+        // Inicializa el DatePicker (se podría hacer con un DatePickerDialog)
+        DatePicker dp = (DatePicker) findViewById(R.id.datePicker);
+        Calendar c = Calendar.getInstance();
+        int year = c.get(java.util.Calendar.YEAR); // Año actual
+        int month = c.get(java.util.Calendar.MONTH); // Mes actual
+        int day = c.get(java.util.Calendar.DAY_OF_MONTH); // Día actual
+
+        dp.init(year, month, day, new DatePicker.OnDateChangedListener() {
+            @Override
+            // El callback devuelve la fecha seleccionada en el DatePicker
+            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                String selectedDate = dayOfMonth + "/" + (monthOfYear + 1) + "/" + year;
+
+                Integer diasDisfrutados = dbHolydays.getFromHolydays(user).size();
+
+                if (diasDisfrutados != null){
+
+                        if (diasDisfrutados < (MAX_DIAS_VACACIONES -1)){
+                            dbHolydays.insertIntoHolydays(selectedDate, user);
+                            Vector<String> holydays = dbHolydays.getFromHolydays(user);
+
+                            Toast t = Toast.makeText(context, print(holydays), Toast.LENGTH_SHORT);
+                            t.show();
+
+
+                        }else if (diasDisfrutados == (MAX_DIAS_VACACIONES - 1)){
+                            dbHolydays.insertIntoHolydays(selectedDate, user);
+                            Vector<String> holydays = dbHolydays.getFromHolydays(user);
+
+                            Toast t = Toast.makeText(context, print(holydays)+ "\n\n\n Has gastado el último dia.", Toast.LENGTH_SHORT);
+                            t.show();
+
+                        }else {
+
+                        Toast t = Toast.makeText(context, "Estos son tua dias: \n" + print(dbHolydays.getFromHolydays(user)), Toast.LENGTH_SHORT);
+                        t.show();
+
+                    }
+                }
+            }
+        });
+    }
+
+    @OnClick (R.id.button)
+    public void deleteFromDatabase (View view) {
+        DBHolydays dbHolydays = new DBHolydays(context);
+        dbHolydays.deleteFromHolydays(user);
+    }
+
+    private String print(Vector<String> holidays) {
+        StringBuffer sb = new StringBuffer();
+        for (int i=0; i < holidays.size(); i++) {
+            sb.append(holidays.elementAt(i));
+            sb.append("\n");
+        }
+
+        return sb.toString();
+    }
+}
